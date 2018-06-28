1078299ddb3b68df16aa65530c0c5c80e1d8cdd2
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-06-28 16:39:52 -0500

    removed avatars added social media icons to footer

--- js/home-page.js
+++ js/home-page.js
@@ -1,9 +1,8 @@
+function averageSales(salesPeople, ){
+    var totalSales = 0;
 
-
-function capitalizeName(str){
-    var names = str.split(" ");
-    for (var i = 0; i < names.length; i += 1){
-        names[i] = names[i][0].toUpperCase() + names[i].slice(1)
-    }
-    return names.join(" ");
+    salesPeople.forEach(function(person){
+        totalSales += person.sales;
+    });
+    console.log(totalSales);
 }
