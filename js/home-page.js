3d206938f8bc2fb63fc4441dd7fb27c671f9e79f
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-06-29 15:33:21 -0500

    commented out wallpaper increased height

--- js/home-page.js
+++
@@ -1,8 +0,0 @@
-function averageSales(salesPeople, ){
-    var totalSales = 0;
-
-    salesPeople.forEach(function(person){
-        totalSales += person.sales;
-    });
-    console.log(totalSales);
-}
