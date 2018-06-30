7d1e08c541076e5408c071db2ff5f090cc43745a
Justin Wayne Wilson <wilsonjustinwayne@gmail.com>
2018-06-30 16:22:08 -0500

    navbar update

--- js/home-page.js
+++ js/home-page.js
@@ -1,8 +1,9 @@
 var konamiKeys = ["38,38,40,40,37,39,37,39,66,65"];
 var inputString = [];
-function konamiCode (input){
+
+function konamiCode(input) {
     inputString.push(input.keyCode);
-    if (inputString.toString().indexOf(konamiKeys) >= 0 ){
+    if (inputString.toString().indexOf(konamiKeys) >= 0) {
         console.log('success');
         // $("h1").css('background','blue');
 
@@ -11,19 +12,30 @@ function konamiCode (input){
 
 }
 
-$(document).keyup(function(event){
+$(document).keyup(function (event) {
     window.addEventListener("keyup", konamiCode);
     console.log(event.keyCode);
 });
 
+function switchALinks(isScrolled) {
+    if (isScrolled) {
+        $('#invis-scroll').css('display', 'none');
+        $('#backToTop').css('display', 'inline');
+    } else {
+        $('#invis-scroll').css('display', 'inline');
+        $('#backToTop').css('display', 'none');
+    }
+}
 
 // this functionality makes the navbar stick to the top and shorten just a bit though a special class
-$(document).ready(function() {
-    $(window).on('scroll', function() {
+$(document).ready(function () {
+    $(window).on('scroll', function () {
         if (Math.round($(window).scrollTop()) > 100) {
             $('.navbar').addClass('scrolled');
+            switchALinks(true);
         } else {
             $('.navbar').removeClass('scrolled');
+            switchALinks(false);
         }
     });
 });
