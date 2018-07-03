5dd00a210533c2273be5116036e5c22d18e0ed64
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-07-03 10:36:49 -0500

    added smooth scrolling js and updated socila media icon colors

--- js/home-page.js
+++ js/home-page.js
@@ -47,3 +47,25 @@ $(document).ready(function () {
         }
     });
 });
+
+// smooth scrolling
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
